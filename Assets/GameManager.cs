07a1cb5b8ef70885c045da5726adf1ee05a3f5b0
SWSTuukka <somewhatweirdsoftworks@outlook.com>
2023-10-12 00:55:08 +0300

    Commit_121023
    Menu screen added, working, adjusted clown jumpscare position (still not good), Fade animations between scene transitions working. Menu buttons working.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Animator fadeScreen;
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(int levelNum)
+    {
+        StartCoroutine(FadeScreenLoad(levelNum));
+    }
+
+    public IEnumerator FadeScreenLoad(int levelNum)
+    {
+        fadeScreen.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1.8f);
+        SceneManager.LoadScene(levelNum);
+    }
+}
