023873bb5c24b4ada4b93dc4e18615535e5dff8b
Essillore <riikka.pehkonen@me.com>
2023-10-11 15:56:32 +0300

    Commit 111023_11 Colour Curves wavescripts
    Pushed my experiments, if our audio/music wizards want to do something with these. Require a postprocessing volume in the scene to work

---
+++ Assets/Scripts/MovingWaveColorCurve.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering.PostProcessing;
+
+public class MovingWaveColorCurve : MonoBehaviour
+{
+    // Reference to the URP Volume component on your camera
+    public PostProcessVolume postProcessVolume;
+    public MeshRenderer cubeRenderer;
+    private ColorGrading colorGrading;
+    //movingWaveCurve;
+    private AnimationCurve waveCurve;
+    private float waveSpeed = 0.5f;
+    private float waveAmplitude = 1.5f;
+
+    private void Start()
+    {
+        // Check if the postProcessingVolume is assigned
+        if (postProcessVolume == null)
+        {
+            Debug.LogError("Post Processing Volume is not assigned!");
+            return;
+        }
+
+        // Try to get the ColorCurves effect from the post-processing profile
+        if (postProcessVolume.profile.TryGetSettings(out ColorGrading cg))
+        {
+            colorGrading = cg;
+        }
+        else
+        {
+            Debug.LogError("Color Curves effect not found in the post-processing profile.");
+            return;
+        }
+
+        // Initialize the wave curve
+        InitializeWaveCurve();
+
+        // Start the wave movement
+        StartCoroutine(MoveWave());
+    }
+
+    // Initialize the wave curve
+    private void InitializeWaveCurve()
+    {
+        waveCurve = new AnimationCurve();
+        // waveCurve.AddKey(new Keyframe(0.0f, 0.0f));
+        // waveCurve.AddKey(new Keyframe(1.0f, 0.0f));
+
+        // Adjust the keyframes to emphasize blue values
+        waveCurve.AddKey(new Keyframe(0.0f, 0.0f));
+        waveCurve.AddKey(new Keyframe(0.141f, 0.710f));         // Transition from black to blue
+        waveCurve.AddKey(new Keyframe(0.355f, 0.240f));          // Blue
+        waveCurve.AddKey(new Keyframe(0.609f, 0.683f));         // Transition from blue to black
+        waveCurve.AddKey(new Keyframe(0.801f, 0.240f));          // End with black
+
+    }
+
+    // Coroutine to move the wave over time
+    private IEnumerator MoveWave()
+    {
+        float time = 0.0f;
+        Color originalColor = Color.white;  // Assuming your wave starts as white
+
+        while (true)
+        {
+            // Update the curve's keyframes to create the moving wave
+            for (int i = 0; i < waveCurve.length; i++)
+            {
+                Keyframe key = waveCurve[i];
+                key.value = Mathf.Sin((key.time + time) * waveSpeed) * waveAmplitude;
+                waveCurve.MoveKey(i, key);
+
+                // Calculate the hue value based on the key's value
+                float hue = Mathf.Lerp(240f, 0f, key.value);  // Blue to Red (adjust the values as needed)
+
+                // Convert hue to RGB color
+                Color newColor = Color.HSVToRGB(hue / 360f, 1f, 1f);
+
+                // Apply the new color to your wave (assuming you have a Renderer)
+                cubeRenderer.material.color = newColor;  // Replace "yourRenderer" with your actual wave's renderer
+            }
+
+            time += Time.deltaTime;
+            yield return null;
+                // Assign the updated curve to the blue channel
+          //      colorGrading.hueShift.value = waveCurve.Evaluate;
+
+
+            }
+        }
+    }
+
---
+++ Assets/Scripts/SpectrumWaveColorCurve.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SpectrumWaveColorCurve : MonoBehaviour
+{
+    // Reference to the URP Volume component on your camera
+    public PostProcessVolume postProcessVolume;
+    public MeshRenderer cubeRenderer;
+    private ColorGrading colorGrading;
+    private AnimationCurve waveCurve;
+    private float waveSpeed = 0.02f;
+    private float waveAmplitude = 0.9f;
+
+    // Audio variables
+    public AudioSource audioSource;
+    public float hueShiftMultiplier = 360f; // Adjust this value to control the range of hue shift
+    public float audioAmplitudeScale = 1.0f;
+
+    private void Start()
+    {
+        // Check if the postProcessingVolume is assigned
+        if (postProcessVolume == null)
+        {
+            Debug.LogError("Post Processing Volume is not assigned!");
+            return;
+        }
+
+        // Try to get the ColorCurves effect from the post-processing profile
+        if (postProcessVolume.profile.TryGetSettings(out ColorGrading cg))
+        {
+            colorGrading = cg;
+        }
+        else
+        {
+            Debug.LogError("Color Curves effect not found in the post-processing profile.");
+            return;
+        }
+
+        // Initialize the wave curve
+        InitializeWaveCurve();
+
+        // Start the wave movement
+        StartCoroutine(MoveWave());
+    }
+
+    // Initialize the wave curve
+    private void InitializeWaveCurve()
+    {
+        waveCurve = new AnimationCurve();
+        waveCurve.AddKey(new Keyframe(0.0f, 0.0f));
+        waveCurve.AddKey(new Keyframe(0.141f, 0.710f));         // Transition from black to blue
+        waveCurve.AddKey(new Keyframe(0.355f, 0.240f));          // Blue
+        waveCurve.AddKey(new Keyframe(0.609f, 0.683f));         // Transition from blue to black
+        waveCurve.AddKey(new Keyframe(0.801f, 0.240f));
+    }
+
+    // Coroutine to move the wave over time
+    private IEnumerator MoveWave()
+    {
+        float time = 0.0f;
+
+        while (true)
+        {
+            // Update the curve's keyframes to create the moving wave
+            for (int i = 0; i < waveCurve.length; i++)
+            {
+                Keyframe key = waveCurve[i];
+                key.value = Mathf.Sin((key.time + time) * waveSpeed) * waveAmplitude;
+                waveCurve.MoveKey(i, key);
+            }
+
+            time += Time.deltaTime;
+
+            // Get audio spectrum data
+            float[] spectrumData = new float[256]; // You can adjust the size as needed
+            audioSource.GetSpectrumData(spectrumData, 0, FFTWindow.BlackmanHarris);
+
+            for (int i = 0; i < spectrumData.Length; i++)
+            {
+                float tmp = spectrumData[i] * 160;
+                if (tmp >= 3f)
+                {
+                    //gameObject.transform.rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+                    Debug.Log(spectrumData[i]);
+                }
+
+                // Calculate the average amplitude from the spectrum data
+                float averageAmplitude = 0f;
+                for (int j = 0; j < spectrumData.Length; j++)
+                {
+                    averageAmplitude += spectrumData[i];
+                }
+                averageAmplitude /= spectrumData.Length;
+
+                // Calculate hue shift based on audio amplitude
+                float hueShift = averageAmplitude * audioAmplitudeScale * hueShiftMultiplier;
+
+                // Apply the hue shift to the Color Grading effect
+                colorGrading.hueShift.value = hueShift;
+
+                yield return null;
+
+            }
+
+        }
+    }
+}
